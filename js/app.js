dfe1a1f4a6c706fab8fb563c7c3072a21c6c2065
Jesus-C-R <jesuscorrearodriguez4@gmail.com>
2026-03-19 21:18:25 +0100

    Refactor app.js to improve readability and structure

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let barraLucidez = document.getElementById("barra-lucidez");
 let toastLucidez = document.getElementById("toast-lucidez");
 let toastLucidezB = new bootstrap.Toast(toastLucidez);
 let spinner = document.querySelector(".spinner-grow");
-// Activar los Popovers del Bestiario
+
 const listaNodosPopovers = document.querySelectorAll('[data-bs-toggle="popover"]');
 const listaPopovers = [...listaNodosPopovers].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
@@ -17,14 +17,11 @@ const listaTooltips = [...listaNodosTooltips].map(nodoTooltip => new bootstrap.T
 function iniciarTransicion() {
     console.log("¡Botón pulsado! Iniciando magia...");
 
-    // 1. Escondemos el botón y revelamos el Spinner de carga
     botonInicio.classList.add("d-none");
     spinner.classList.remove("d-none");
 
-    // 2. ¡La magia de la transición vuelve! Activamos el desvanecimiento
     pantallaInicio.classList.add("desvanecer");
     
-    // 3. Esperamos 2 segundos y cambiamos de pantalla
     setTimeout(function () {
         pantallaInicio.classList.remove("d-flex"); 
         pantallaInicio.classList.add("d-none");    
@@ -32,13 +29,10 @@ function iniciarTransicion() {
     }, 2000);
 };
 
-// He cambiado el nombre de la función por si "transition" daba conflicto con el navegador
 botonInicio.addEventListener("click", iniciarTransicion);
 
-// --- LÓGICA DEL MANUSCRITO (MODAL) ---
 const modalLore = document.getElementById('modalLore');
 
-// 2. Le decimos a Bootstrap que escuche el evento 'show.bs.modal' (cuando el modal empieza a abrirse)
 modalLore.addEventListener('show.bs.modal', function (event) {
 
     let puntoRojo = event.relatedTarget;
@@ -49,9 +43,8 @@ modalLore.addEventListener('show.bs.modal', function (event) {
     let zonaTitulo = document.getElementById('tituloModal');
     let zonaCuerpo = document.getElementById('cuerpoModal');
 
-    // Inyectamos el texto del punto rojo dentro de la ventana Modal
     zonaTitulo.textContent = tituloHistoria;
-    zonaCuerpo.innerHTML = textoHistoria; // Usamos innerHTML porque tu texto tiene etiquetas <br> y <span>
+    zonaCuerpo.innerHTML = textoHistoria;
 
     if (puntoRojo.classList.contains("leido")) {
         console.log("Lugar ya descubierto");
@@ -70,30 +63,23 @@ modalLore.addEventListener('show.bs.modal', function (event) {
     }
 });
 
-// --- LÓGICA DEL ARSENAL (MODAL DE ARMAS) ---
-
 const modalArma = document.getElementById('modalArma');
 
 modalArma.addEventListener('show.bs.modal', function (event) {
-    // 1. Saber qué botón exacto se ha pulsado
     let botonArma = event.relatedTarget;
 
-    // 2. Extraer la información de la "mochila" del botón
     let nombre = botonArma.getAttribute('data-arma-nombre');
     let descripcion = botonArma.getAttribute('data-arma-desc');
     let imagenRuta = botonArma.getAttribute('data-arma-img');
 
-    // 3. Identificar las zonas vacías del Modal Central
     let zonaTitulo = document.getElementById('tituloArma');
     let zonaTexto = document.getElementById('textoArma');
     let zonaImagen = document.getElementById('imagenArma');
 
-    // 4. Inyectar la información
     zonaTitulo.textContent = nombre;
     zonaTexto.textContent = descripcion;
-    zonaImagen.src = imagenRuta; // ¡Cambiamos el atributo src de la etiqueta img!
+    zonaImagen.src = imagenRuta;
 
-    // 5. Cerrar automáticamente el Offcanvas (Menú lateral)
     let menuLateral = bootstrap.Offcanvas.getInstance(document.getElementById('menuArsenal'));
     if (menuLateral) {
         menuLateral.hide();
